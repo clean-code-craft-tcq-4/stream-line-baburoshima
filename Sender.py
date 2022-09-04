aabd2dbf5880680a52b1bfdba1591e3beffe537e
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-09-04 19:17:03 +0530

    Adding more test cases

--- Sender.py
+++ Sender.py
@@ -1,32 +1,45 @@
 import csv
 import random
+from pathlib import Path
 
-def WriteSensorInput():
-    with open('SensorInput.csv', mode='w',newline='') as SensorInput:
+PARAMETER1_MIN = 0
+PARAMETER1_MAX = 45
+PARAMETER2_MIN = 20
+PARAMETER2_MAX = 80
+Sensor_ReadingCount = 50
+Parameter_name =  ['Temperature' , 'SOC']
+
+def CheckifFileExists(filename):
+    path = Path(filename)
+    return True if path.is_file() else False
+
+def WriteSensorReadings(file = 'SensorReadings.csv'):
+    with open(file, mode='w',newline='') as SensorInput:
         SensorInput = csv.writer(SensorInput, delimiter=',')
-        SensorInput.writerow(['Temperature' , 'SOC'])
-        for item in range(0,50):
-            item = [random.randint(0,45), random.randint(20,80)]
+        SensorInput.writerow(Parameter_name)
+        for item in range(0,Sensor_ReadingCount):
+            item = [random.randint(PARAMETER1_MIN,PARAMETER1_MAX), random.randint(PARAMETER2_MIN,PARAMETER2_MAX)]
             SensorInput.writerow(item)
 
-def ReadSensorInput():
-    WriteSensorInput()
-    with open('SensorInput.csv', mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        BatteryParameters = []
-        BatteryParamtersValues = []
-        for row in csv_reader:
-            if line_count == 0:
-                BatteryParameters = f'{", ".join(row)}'
-                print(BatteryParameters)
-                line_count += 1
-            else:
-                BatteryParamtersValues = f'{row[0]},{row[1]}'
-                print(BatteryParamtersValues)
-                line_count += 1
-    return BatteryParameters,BatteryParamtersValues
+def ReadSensorReadings(file = 'SensorReadings.csv'):
+    if CheckifFileExists(file):
+        BatteryParameter1_Values = []
+        BatteryParameter2_Values = []
+        with open(file, mode='r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            BatteryParameters = next(csv_reader) 
+            print(f'{", ".join(BatteryParameters)}')
+            for row in csv_reader:
+                print(f'{row[0]},{row[1]}')
+                BatteryParameter1_Values.append(row[0])
+                BatteryParameter2_Values.append(row[1])
+            return BatteryParameters,BatteryParameter1_Values, BatteryParameter2_Values
+    else :
+        return None
 
+#file = 'SensorReadings.csv'
+#WriteSensorInput(file)
+#ReadSensorReadings(file)
 
 
 
--- testSender.py
+++ testSender.py
@@ -1,8 +1,43 @@
 import unittest
 from Sender import *
+import csv 
+from pathlib import Path
+
+InvalidRange_file = 'InvalidRange.csv'
+rowsInvalid = [['Temperature','SOC'],['46','52'],['27','81']]
+generated_file = 'Generated.csv'
 
 class BMS_Sender(unittest.TestCase):
+  def test_Writecsv(self):
+      WriteSensorReadings('SensorInput.csv')
+      self.assertTrue(CheckifFileExists('SensorInput.csv')==True)
+
+  def test_IfFileExists(self):    
+     self.assertTrue(CheckifFileExists('NonExisting.csv')==False)
+     with open(generated_file, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerows( ['Value1','Value2'])
+     self.assertTrue(CheckifFileExists(generated_file)==True)
+     Path(generated_file).unlink()
+
   def test_readcsv(self):
-    self.assertTrue(ReadSensorInput()[0]=="Temperature, SOC")
+      self.assertTrue(ReadSensorReadings('SensorInput.csv')[0]==['Temperature', 'SOC'])
+      self.assertTrue(0 <= int(ReadSensorReadings('SensorInput.csv')[1][0]) <= 45)
+      self.assertTrue(20 <= int(ReadSensorReadings('SensorInput.csv')[2][1])<= 80)
+
+  def test_InvalidRanges(self):
+      with open(InvalidRange_file, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerows(rowsInvalid)
+      self.assertTrue(ReadSensorReadings(InvalidRange_file)[0]==['Temperature', 'SOC'])
+      self.assertFalse(0 <= int(ReadSensorReadings(InvalidRange_file)[1][0]) <= 45)
+      self.assertFalse(20 <= int(ReadSensorReadings(InvalidRange_file)[2][1])<= 80)
+      self.assertTrue(0 <= int(ReadSensorReadings(InvalidRange_file)[1][1]) <= 45)
+      self.assertTrue(20 <= int(ReadSensorReadings(InvalidRange_file)[2][0])<= 80)
+      Path(InvalidRange_file).unlink()
+
+  def test_NonExistingcsv(self):
+      self.assertTrue(ReadSensorReadings('NonExisting.csv')==None)
+
 
 unittest.main()
