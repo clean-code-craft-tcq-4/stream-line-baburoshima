8a993f1d120be5aa4298ddd110148d08b4c77cf9
Naik Punith <pnu5kor@bosch.com>
2022-09-19 10:32:10 +0530

    Add Receiver code

--- TestReceiver.py
+++ TestReceiver.py
@@ -7,54 +7,31 @@ class TestReceiver(unittest.TestCase):
 
     #Test remove_invalid_value with InvalidRange
     def test_remove_invalid_value_with_InvalidRange(self):
-        input_list = ["34","43","InvalidRange","55","InvalidRange"]
-        expected_list = ["34","43","55"]
-        actual_list = remove_invalid_value(input_list)
-        self.assertEqual(actual_list,expected_list)
+        self.assertEqual(remove_invalid_value(["34","43","InvalidRange","55","InvalidRange"]),["34","43","55"])
 
     #Test remove_invalid_value without InvalidRange
     def test_remove_invalid_value_without_InvalidRange(self):
-        input_list = ["34","43","55"]
-        expected_list = ["34","43","55"]
-        actual_list = remove_invalid_value(input_list)
-        self.assertEqual(actual_list,expected_list)
+        self.assertEqual(remove_invalid_value(["43","4","22"]),["43","4","22"])
 
     #Test get_valid_data
     def get_valid_data(self):
-        input_list = ["Temperature","34","43","55","InvalidRange"]
-        expected_list = ["34","43","55"]
-        actual_list = get_valid_data(input_list)
-        self.assertEqual(actual_list,expected_list)
+        self.assertEqual(get_valid_data(["Temperature","3","13","17","InvalidRange"]),["3","13","17"])
     
     #Test get_data_from_input_stream with valid values
     def test_get_data_with_valid_values(self):
-        input_stream = ['Temperature, SOC\n', '37,21\n', '20,31\n', '45,50\n', '40,30\n', '35,50\n'] 
-        sensor1,sensor1_values,sensor2,sensor2_values = get_data_from_input_stream(input_stream)
-        self.assertEqual(sensor1,"Temperature")
-        self.assertEqual(sensor1_values,[37,20,45,40,35])
-        self.assertEqual(sensor2,"SOC")
-        self.assertEqual(sensor2_values,[21,31,50,30,50])
+        self.assertEqual(get_data_from_input_stream(['Temperature, SOC\n', '37,21\n', '20,31\n', '45,50\n', '40,30\n', '35,50\n']),("Temperature",[37,20,45,40,35],"SOC",[21,31,50,30,50]))
 
     #Test get_data_from_input_stream with invalid values
     def test_get_data_with_invalid_values(self):
-        input_stream = ['Temperature, SOC\n', '37,21\n', 'Invalid Range,78\n', '20,31\n', '45,Invalid Range\n', '40,30\n', '35,50\n'] 
-        sensor1,sensor1_values,sensor2,sensor2_values = get_data_from_input_stream(input_stream)
-        self.assertEqual(sensor1,"Temperature")
-        self.assertEqual(sensor1_values,[37,20,45,40,35])
-        self.assertEqual(sensor2,"SOC")
-        self.assertEqual(sensor2_values,[21,78,31,30,50])
+        self.assertEqual(get_data_from_input_stream(['Temperature, SOC\n', '37,21\n', 'Invalid Range,78\n', '20,31\n', '45,Invalid Range\n', '40,30\n', '35,50\n']),("Temperature",[37,20,45,40,35],"SOC",[21,78,31,30,50]))
 
     #Test get_max_min_value list with values
     def test_get_max_min_list_values(self):
-        min_val,max_val = get_max_min_value([12,23,52,22])
-        self.assertEqual(min_val,12)
-        self.assertEqual(max_val,52)
+        self.assertEqual(get_max_min_value([12,23,52,22]),(12,52))
 
     #Test get_max_min_value with empty list
     def test_get_max_min_empty_list(self):
-        min_val,max_val = get_max_min_value([])
-        self.assertEqual(min_val,None)
-        self.assertEqual(max_val,None)
+        self.assertEqual(get_max_min_value([]),(None,None))
 
     #Test print_max_min list with values
     #Mocking print function
@@ -117,9 +94,9 @@ class TestReceiver(unittest.TestCase):
     #Mocking print function
     @patch('builtins.print')
     def test_print_sma_list_equal_count(self,mock_print):
-        print_simple_moving_average("Temperature",[12,22,32,42,2],"SOC",[10,20,25,30,35])
-        self.assertEqual(mock_print.mock_calls,[call("Simple Moving Average of last 5 values of Temperature is 22.0"),
-                                                call("Simple Moving Average of last 5 values of SOC is 24.0")])
+        print_simple_moving_average("Temperature",[12,22,32,42,11],"SOC",[10,20,25,30,15])
+        self.assertEqual(mock_print.mock_calls,[call("Simple Moving Average of last 5 values of Temperature is 23.8"),
+                                                call("Simple Moving Average of last 5 values of SOC is 20.0")])
 
     #Test print_simple_moving_average both list with values < COUNT_FOR_SMA
     #Mocking print function
@@ -133,16 +110,16 @@ class TestReceiver(unittest.TestCase):
     #Mocking print function
     @patch('builtins.print')
     def test_print_sma_templist_less_count(self,mock_print):
-        print_simple_moving_average("Temperature",[22,32,42,2],"SOC",[10,20,25,30,35])
+        print_simple_moving_average("Temperature",[22,32,42,5],"SOC",[10,20,25,30,3])
         self.assertEqual(mock_print.mock_calls,[call("Number of values in Temperature list is less than 5"),
-                                                call("Simple Moving Average of last 5 values of SOC is 24.0")])
+                                                call("Simple Moving Average of last 5 values of SOC is 17.6")])
 
     #Test print_simple_moving_average only SOC list with values < COUNT_FOR_SMA
     #Mocking print function
     @patch('builtins.print')
     def test_print_sma_soclist_less_count(self,mock_print):
-        print_simple_moving_average("Temperature",[12,22,32,42,2],"SOC",[20,25,30,35])
-        self.assertEqual(mock_print.mock_calls,[call("Simple Moving Average of last 5 values of Temperature is 22.0"),
+        print_simple_moving_average("Temperature",[15,22,32,42,2],"SOC",[10,25,30,35])
+        self.assertEqual(mock_print.mock_calls,[call("Simple Moving Average of last 5 values of Temperature is 22.6"),
                                                 call("Number of values in SOC list is less than 5")])
                                                 
     #Test main_func with no values
@@ -158,7 +135,7 @@ class TestReceiver(unittest.TestCase):
     @patch('Receiver.print_max_min')
     @patch('Receiver.print_simple_moving_average')
     def test_main_func_with_values(self,mock_print_simple_moving_average,mock_print_max_min,mock_get_data_from_input_stream):
-        input_stream = "Temperature, SOC\n37,21\n5,78\n20,31\n45,50\n40,30\n35,50"
+        input_stream = "Temperature, SOC\n57,21\n5,88\n20,31\n45,50\n40,30\n35,50"
         main_func(input_stream)
         self.assertEqual(mock_get_data_from_input_stream.call_count,1)
         self.assertEqual(mock_print_max_min.call_count,1)
